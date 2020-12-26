301f81e08ef9257fd4afe79f3d8822a06098b3cb
IKK-LTU <ignas.kunickas@gmail.com>
2020-12-26 14:06:29 +0200

    fix

--- src/Components/Main/services/ServiceList/ServiceList.js
+++ src/Components/Main/services/ServiceList/ServiceList.js
@@ -1,30 +1,28 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
 import classes from './ServiceList.css'
 
-const ServicesList = (headline, text) => {
+
+const ServicesList = ({ headline, text }) => {
   return (
-    <div className={classes.ServiceList}>
-      <h4 style={{ marginBottom: '10px' }}>{headline}</h4>
-
-      <p>
-        {' '}
-        {text}
-        {/* <strong class="strong">DAUGIAU!</strong>
-                     <i class="ion-arrow-forward-circle-outline"></i> */}
-        ...
-        <strong
-          style={{
-            color: '#99db48',
-            textTransform: 'uppercase',
-
-            letterSpacing: '0.5px',
-          }}
-        >
-          DAUGIAU!
-        </strong>
-      </p>
-    </div>
-  )
+  <div className={classes.ServiceListt}>
+    <h4 style={{ marginBottom: '10px' }}>{headline}</h4>
+    <p>
+      {text}
+      <strong
+        style={{
+          color: '#99db48',
+          textTransform: 'uppercase',
+          letterSpacing: '0.5px',
+        }}>
+          
+        DAUGIAU!
+      </strong>
+    </p>
+  </div>
+)}
+ServicesList.propTypes = {
+  headline: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 }
 export default ServicesList
--- src/Components/Main/services/Services.js
+++ src/Components/Main/services/Services.js
@@ -4,61 +4,40 @@ import Wall from '../../Img/siena.jpeg'
 import classes from './Services.css'
 import ServiceList from './ServiceList/ServiceList'
 
-
 function Services() {
-const Service = [
-  {
-    headline: 'Betonavimo Darbai',
-    Text:
-      'Pamatai gali būti gręžtiniai, juostiniai, stulpiniai, priklausomai nuo statomo pastato, būsimųjo apkrovų, grunto ypatybių. Kokie pamatai bus įrengiami, numatoma pastato projekte atlikus grunto tyrimus...',
-    id: 1,
-  },
-
-  {
-    headline: 'TVOROS IR TERASOS',
-    Text:
-      'Pamatai gali būti gręžtiniai, juostiniai, stulpiniai,priklausomai nuo statomo pastato, būsimųjo apkrovų, grunto ypatybių...',
-    id: 2,
-  },
-
-  {
-    headline: 'TERASOS, BEI PAVĖSINĖS',
-    Text:
-      'Pamatai gali būti gręžtiniai, juostiniai, stulpiniai, priklausomai nuo statomo pastato, būsimųjo apkrovų, grunto ypatybių...',
-    id: 3,
-  },
-
-  {
-    headline: 'TRINKELIU KLOJIMAS',
-    Text:
-      'Pamatai gali būti gręžtiniai, juostiniai, stulpiniai, priklausomai nuo statomo pastato...',
-    id: 4,
-  },
-  {
-    headline: 'APDAILOS DARBAI',
-    Text: 'aaa',
-    id: 5,
-  },
-  ]
-  
-
-    return (
-      <div
-        className={classes.Services}
-        style={{
-          backgroundAttachment: 'fixed',
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)),url(${Wall})`,
-        }}
-      >
-        <h2> MŪSŲ ATLIEKAMI DARBAI</h2>
-        <div className={classes.ServiceList}>
-          {Service.map((Servicee) => (
-            <ServiceList key={Servicee.id} headline={Servicee.headline} text={Servicee.Text} />
-          ))}
-        </div>
+  return (
+    <div
+      className={classes.ServicesStyle}
+      style={{
+        backgroundAttachment: 'fixed',
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7),rgba(0, 0, 0, 0.7)),url(${Wall})`,
+      }}
+    >
+      <h2> MŪSŲ ATLIEKAMI DARBAI</h2>
+      <div className={classes.SserviceList}>
+        <ServiceList
+          headline="Betonavimo Darbai"
+          text="Pamatai gali būti gręžtiniai, juostiniai, stulpiniai, priklausomai nuo statomo pastato, būsimųjo apkrovų, grunto ypatybių. Kokie pamatai bus įrengiami, numatoma pastato projekte atlikus grunto tyrimus"
+        />
+        <ServiceList
+          headline="TVOROS IR TERASOS"
+          text="Pamatai gali būti gręžtiniai, juostiniai, stulpiniai,priklausomai nuo statomo pastato, būsimųjo apkrovų, grunto ypatybių..."
+        />
+        <ServiceList
+          headline="TERASOS, BEI PAVĖSINĖS"
+          text="Pamatai gali būti gręžtiniai, juostiniai, stulpiniai, priklausomai nuo statomo pastato..."
+        />
+        <ServiceList
+          headline="TRINKELIU KLOJIMAS"
+          text="Pamatai gali būti gręžtiniai, juostiniai, stulpiniai, priklausomai nuo statomo pastato..."
+        />
+        <ServiceList
+          headline="APDAILOS DARBAI"
+          text="Pamatai gali būti gręžtiniai, juostiniai, stulpiniai, priklausomai nuo statomo pastato..."
+        />
       </div>
-    )
-  
+    </div>
+  )
 }
 
 export default Services
